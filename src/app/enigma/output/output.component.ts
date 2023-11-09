4ff7d4aa88a013b6af574f82a98207e8def3a73d
Pawel Maslak <maslak.pawel89@gmail.com>
2023-11-09 23:33:23 +0100

    Bound output

--- src/app/enigma/output/output.component.ts
+++ src/app/enigma/output/output.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeyEventsService } from 'src/app/services/key-events.service';
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
   styleUrls: ['./output.component.scss'],
 })
-export class OutputComponent {
-  public outputContent: string = 'This is sample Enigma output';
+export class OutputComponent implements OnInit {
+  public outputContent: string = '';
+  public outputLetters: string[] = [];
+
+  constructor(private keyEventsService: KeyEventsService) {}
+
+  ngOnInit(): void {
+    this.keyEventsService.keyProcessed$.subscribe((key) => {
+      // Process the key press event here, e.g., call a method
+      this.processKey(key);
+    });
+  }
+
+  private processKey(key: string): void {
+    this.outputLetters.push(key);
+    this.outputContent = this.outputLetters.join('');
+  }
 }
