9153a3acc510c400e510aa6668b459d5e1fac31b
Pawel Maslak <pawel.maslak@bystronic.com>
2023-11-10 12:14:52 +0100

    Added new layout of control panel

--- src/app/enigma/control-panel/control-panel.component.ts
+++ src/app/enigma/control-panel/control-panel.component.ts
@@ -10,7 +10,28 @@ import { RotorSection } from 'src/app/models/rotor-section';
 export class ControlPanelComponent {
   @Input() plugboard: Plugboard;
   @Input() rotorSection: RotorSection;
+  public machineConfigurationAvailable: boolean = false;
   public ringSettingsVisible: boolean = false;
+
+  public getRingSettingsText(): string {
+    return this.ringSettingsVisible ? 'Set Rotor Position' : 'Set Ring Settings';
+  }
+
+  public loadMachineSettings(): void {
+    alert('Settings loaded');
+  }
+
+  public resetSettings(): void {
+    //Calculate steps
+    //Calculate ring settings
+    //Remove all encoded text
+    //Remove plugs
+  }
+
+  public saveMachineSettings(): void {
+    this.machineConfigurationAvailable = true;
+    alert('Settings saved');
+  }
 
   public toggleRingSettings(): void {
     this.ringSettingsVisible = !this.ringSettingsVisible;
