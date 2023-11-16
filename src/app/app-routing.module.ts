034486b3917bf1753e0eb1e3e9bac4b5b7f16636
Pawel Maslak <maslak.pawel89@gmail.com>
2023-11-16 17:55:50 +0100

    Added basic routing and background settings to allow for longer and wider content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnigmaComponent } from './enigma/enigma.component';
+import { WikiComponent } from './support/wiki/wiki.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'wiki', component: WikiComponent },
+  { path: 'enigma', component: EnigmaComponent },
+  { path: '', redirectTo: '/enigma', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
